1eb37db28c67129bd73a37627d4ecfb8af41da1f
Erllyta Rachma <erllyta107@gmail.com>
2025-05-30 09:09:10 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News App with Comments',
+      title: 'News App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
